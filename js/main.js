22c0a08debe7c6249d99a03b7c0e5523afc3316f
Nicholas Cafaro <nicholas.cafaro@gmail.com>
2019-02-18 20:27:41 -0500

    <project setup and minor fixes to get page loaded. Added paths to data for restaurant.json Added Mapbox token>

--- js/main.js
+++ js/main.js
@@ -78,7 +78,7 @@ initMap = () => {
         scrollWheelZoom: false
       });
   L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.jpg70?access_token={mapboxToken}', {
-    mapboxToken: '<your MAPBOX API KEY HERE>',
+    mapboxToken: 'pk.eyJ1IjoibmRyYzgxIiwiYSI6ImNqc2FoZXp0azBuOG40YXJ6MmE3aGY4YWwifQ.Q0gvTEU9czZ3e8VIo7onnw',
     maxZoom: 18,
     attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
       '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
